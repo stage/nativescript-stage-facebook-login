bd0ec9db26bdbb56f3fe4b5948a54ec3562dfd85
Blake Nussey <blake.nussey@gmail.com>
2017-05-23 11:07:48 -0700

    First commit

---
+++ facebook-handler.android.ts
@@ -0,0 +1,97 @@
+//NativeScript modules
+import applicationModule = require("application");
+
+declare var AccessToken: any;
+
+var _isInit: boolean = false;
+var _AndroidApplication = applicationModule.android;
+var _act: android.app.Activity;
+
+var mCallbackManager;
+var loginManager;
+
+// not implemented
+export function isAlreadyAuthenticated(): boolean {
+  return com.facebook.AccessToken.getCurrentAccessToken() != null;
+}
+
+// not implemented
+export function accessToken(): boolean {
+  return com.facebook.AccessToken.getCurrentAccessToken();
+}
+
+
+export function init(facebookAppId, facebookAppDisplayName): boolean {
+
+  try {
+    //fb initialization
+    com.facebook.FacebookSdk.sdkInitialize(_AndroidApplication.context.getApplicationContext());
+  }
+  catch (e) {
+    console.log("nativescript-facebook-login: The plugin could not find the android library, try to clean the android platform")
+  }
+
+  mCallbackManager = com.facebook.CallbackManager.Factory.create();
+  loginManager = com.facebook.login.LoginManager.getInstance();
+
+  //This solve the case when user changes accounts error code 304
+  loginManager.logOut();
+
+  
+  // if we want to change the loginBehavior:
+  // https://developers.facebook.com/docs/reference/android/current/class/LoginBehavior/
+  // loginManager = loginManager.setLoginBehavior(loginBehavior);
+  
+  if (mCallbackManager && loginManager) {
+    _isInit = true;
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+export function registerCallback(successCallback: any, cancelCallback: any, failCallback: any) {
+
+  if (_isInit) {
+    var act = _AndroidApplication.foregroundActivity || _AndroidApplication.startActivity;
+    _act = act;
+
+    loginManager.registerCallback(mCallbackManager, new com.facebook.FacebookCallback({
+
+      onSuccess: function (result) {
+        successCallback(result.getAccessToken().getToken());
+      },
+      onCancel: function () {
+        cancelCallback();
+
+      },
+      onError: function (e) {
+        failCallback(e);
+      }
+
+    }));
+
+    //Overriding Activity onActivityResult method to send it to the callbackManager
+    act.onActivityResult = (requestCode: number, resultCode: number, data: android.content.Intent) => {
+      mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+  }
+}
+
+export function logInWithPublishPermissions(permissions: string[]) {
+  if (_isInit) {
+    var javaPermissions = java.util.Arrays.asList(permissions);
+    //Start the login process
+    loginManager.logInWithPublishPermissions(_act, javaPermissions);
+  }
+}
+
+export function logInWithReadPermissions(permissions: string[]) {
+  if (_isInit) {
+    var javaPermissions = java.util.Arrays.asList(permissions);
+    //Start the login process
+    loginManager.logInWithReadPermissions(_act, javaPermissions);
+  }
+}
+
---
+++ facebook-handler.ios.ts
@@ -0,0 +1,102 @@
+//NativeScript modules
+import applicationModule = require("application");
+
+var _isInit: boolean = false;
+
+var mCallbackManager;
+var loginManager;
+
+declare var FBSDKLoginManager: any;
+declare var FBSDKLoginBehaviorSystemAccount: any;
+declare var FBSDKLoginBehaviorNative: any;
+declare var FBSDKSettings: any;
+declare var FBSDKAccessToken: any;
+  console.log("facebook handler v3.0");
+
+export function isAlreadyAuthenticated(): boolean {
+  return FBSDKAccessToken.currentAccessToken() ? true : false;
+}
+
+export function accessToken(): any {
+  return FBSDKAccessToken.currentAccessToken();
+}
+
+export function init(facebookAppId, facebookAppDisplayName): boolean { 
+  console.log("facebook handler v3.0 init");
+
+  // set the settings
+  FBSDKSettings.setAppID(facebookAppId);
+  FBSDKSettings.setDisplayName(facebookAppDisplayName);
+
+  // fb initialization
+  loginManager = FBSDKLoginManager.alloc().init();
+
+
+  if (loginManager) {
+    //This solve the case when user changes accounts error code 304
+    loginManager.logOut();
+
+    // loginManager.loginBehavior = FBSDKLoginBehaviorSystemAccount;
+    loginManager.loginBehavior = FBSDKLoginBehaviorNative;
+    
+    _isInit = true;
+    return true;
+   }
+   else {
+    return false;
+   }
+  }
+
+export function registerCallback(successCallback: any, cancelCallback: any, failCallback: any) {
+    if (_isInit) {
+      mCallbackManager= function(result: FBSDKLoginManagerLoginResult, error: NSError) {
+
+        if (error) {
+          console.log("mCallbackManager error: " + error);
+          failCallback(error);
+          return;
+        } 
+        //something went really wrong no error and no result
+        if (!result) {
+          console.log("mCallbackManager !result");
+          failCallback("Null error");
+          return;
+        }
+
+        if (result.isCancelled) {
+          console.log("mCallbackManager result.isCancelled");
+          cancelCallback();
+          return;
+        }
+
+        if (result.token) {
+          console.log("mCallbackManager success: " + result.token.tokenString);
+          successCallback(result.token.tokenString);
+        }
+        else {
+          console.log("mCallbackManager Could not acquire an access token");
+          failCallback("Could not acquire an access token");
+          return;
+        }
+      }
+    }
+  }
+export function logInWithPublishPermissions(permissions: string[]) {
+    if (_isInit) {
+      loginManager.logInWithPublishPermissionsHandler(permissions, mCallbackManager);
+    }
+  }
+export function logInWithReadPermissions(permissions: string[]) {
+    if (_isInit) {
+    loginManager.logInWithReadPermissionsHandler(permissions, mCallbackManager);
+    }
+}
+export function hasGrantedPermission(permission: string) {
+    if (!_isInit) return false;
+    if (!this.isAlreadyAuthenticated()) return;  
+    console.log("hasGrantedPermissions: " + permission);
+    return loginManager.hasGranted(permission);
+}
+
+
+
